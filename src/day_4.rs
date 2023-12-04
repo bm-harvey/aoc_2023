1e663ae40fe8cfd5b615d39e48ce594c42574775
bmharvey <bmharvey@tamu.edu>
2023-12-04 13:44:43 -0600

    cleaned day 4

--- src/day_4.rs
+++ src/day_4.rs
@@ -3,51 +3,26 @@ use crate::solution::Solution;
 pub struct Day4;
 impl Solution for Day4 {
     fn solution(raw_data: &str) -> (u32, u32) {
+        // first pass - solve part 1 and count number of lines
         let mut num_lines = 0;
         let sol_1: u32 = raw_data
             .lines()
             .map(|line| {
-                let start = line.find(':').unwrap();
-                let sets = line[(start + 1)..].split('|').collect::<Vec<_>>();
-
-                let winning_numbers = sets[0]
-                    .split_whitespace()
-                    .map(|s| s.parse::<u32>().unwrap())
-                    .collect::<Vec<_>>();
-
-                let gotten_numbers = sets[1]
-                    .split_whitespace()
-                    .map(|s| s.parse::<u32>().unwrap())
-                    .collect::<Vec<_>>();
+                let (winning_nums, gotten_nums) = Day4::unpack_line(line);
                 num_lines += 1;
-                Day4::score(&winning_numbers, &gotten_numbers)
+                Day4::score(&winning_nums, &gotten_nums)
             })
             .sum();
 
+        // second pass - solve part 2
         let mut weights = vec![1; num_lines];
-
         let mut idx = 0;
         raw_data.lines().for_each(|line| {
-            let start = line.find(':').unwrap();
-            let sets = line[(start + 1)..].split('|').collect::<Vec<_>>();
-
-            let winning_numbers = sets[0]
-                .split_whitespace()
-                .map(|s| s.parse::<u32>().unwrap())
-                .collect::<Vec<_>>();
-
-            let gotten_numbers = sets[1]
-                .split_whitespace()
-                .map(|s| s.parse::<u32>().unwrap())
-                .collect::<Vec<_>>();
-
-            Day4::weighted_score(&winning_numbers, &gotten_numbers, idx, &mut weights);
+            let (winning_nums, gotten_nums) = Day4::unpack_line(line);
+            Day4::adjust_scores(&winning_nums, &gotten_nums, idx, &mut weights);
             idx += 1;
         });
 
-        for w in weights.iter() {
-            dbg!(w);
-        }
         let sol_2 = weights.iter().sum::<u32>();
         (sol_1, sol_2)
     }
@@ -66,7 +41,7 @@ impl Day4 {
             0_u32
         }
     }
-    fn weighted_score(
+    fn adjust_scores(
         winning_numbers: &[u32],
         gotten_numbers: &[u32],
         idx: usize,
@@ -81,5 +56,19 @@ impl Day4 {
         for w in &mut weights[(idx + 1)..=(idx + num_matches)] {
             *w += increment;
         }
+    }
+
+    fn unpack_line(line: &str) -> (Vec<u32>, Vec<u32>) {
+        let start = line.find(':').unwrap();
+        let sets = line[(start + 1)..].split('|').collect::<Vec<_>>();
+        let winning_nums = sets[0]
+            .split_whitespace()
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+        let gotten_nums = sets[1]
+            .split_whitespace()
+            .map(|s| s.parse::<u32>().unwrap())
+            .collect::<Vec<_>>();
+        (winning_nums, gotten_nums)
     }
 }
